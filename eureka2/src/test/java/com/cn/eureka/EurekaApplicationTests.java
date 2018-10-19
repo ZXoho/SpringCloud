636a52f91111d7ccdd8aa36b2920734378b390de
ZXoho <348105852@qq.com>
2018-10-19 23:49:00 +0800

    更新eureka

--- eureka2/src/test/java/com/cn/eureka/EurekaApplicationTests.java
+++ eureka2/src/test/java/com/cn/eureka/EurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.imooc.eureka;
+package com.cn.eureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class EurekaApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
